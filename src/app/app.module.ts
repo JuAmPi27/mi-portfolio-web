f6fdfe9705888e232aebad7f89817737be190594
Juampi27 <Jpbarnetche27@gmail.com>
2022-01-21 18:29:38 -0300

    Creación de componentes y separación del contenido en cada uno de los mismos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DatosPersonalesComponent } from './datos-personales/datos-personales.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MisProyectosComponent } from './mis-proyectos/mis-proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DatosPersonalesComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    MisProyectosComponent
   ],
   imports: [
     BrowserModule
